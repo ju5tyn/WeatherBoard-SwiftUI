fd8249e8729bfd198f796994b74c49398c1c07e0
Justyn <justynhenman@me.com>
2020-11-05 16:00:02 +0000

    Initial Commit

---
+++ WeatherBoard-SwiftUI/WeatherBoard_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherBoard_SwiftUIApp.swift
+//  WeatherBoard-SwiftUI
+//
+//  Created by Justyn Henman on 05/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct WeatherBoard_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
